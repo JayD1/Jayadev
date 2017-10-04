8aa0e9aa0193085b14de873f3c0fbf0b7a5dda3a
JayD1 <jayadevnaram@gmail.com>
2017-10-04 15:48:53 +0530

    Automation1

---
+++ lazy_git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo Type username password and commit msg
+read com_msg
+#read usr
+#read pswrd
+var1=`git add .`
+var2=`git commit -m "$com_msg"`
+var3=`git push`
+
+echo $var1; $var2; $var3
